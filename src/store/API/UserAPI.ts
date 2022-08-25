b93724aa3c6506e30357deaaf95c61049541082c
Mohiroo <mohiro@bk.ru>
2022-08-26 02:52:55 +0300

    add API actions

---
+++ src/store/API/UserAPI.ts
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { IAccount } from "../../model/IAccount";
+import { ILogin } from "../../model/ILogin";
+
+export const UserAPI = createApi({
+  reducerPath: "userAPI",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000",
+  }),
+  endpoints: (build) => ({
+    getLogin: build.query<ILogin, string>({
+      query: (formLogin) => ({
+        url: "users",
+        params: {
+          login: formLogin,
+        },
+      }),
+      transformResponse: (serverLogins: ILogin[]): ILogin => {
+        return serverLogins[0]
+      },
+    }),
+    addUser: build.mutation<ILogin, ILogin>({
+      query: (user) => ({
+        url: "users",
+        method: "POST",
+        body: user,
+      }),
+    }),
+    getAccount: build.query<IAccount, string>({
+      query: (name) => ({
+        url: "accounts",
+        params: {
+          name: name,
+        },
+      }),
+      transformResponse: (serverAccounts: IAccount[]): IAccount => {
+        return serverAccounts[0]
+      },
+    }),
+    addAccount: build.mutation<IAccount, IAccount>({
+      query: (account) => ({
+        url: "accounts",
+        method: "POST",
+        body: account,
+      }),
+    }),
+  }),
+});
