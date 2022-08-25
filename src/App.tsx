15151a01015964ef9433218c6701d61549539e8b
Mohiroo <mohiro@bk.ru>
2022-08-26 02:56:45 +0300

    add layout with privite links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import { Button, Layout } from "antd";
+import { Header, Content } from "antd/lib/layout/layout";
+import React, { useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.scss";
+import MyHeader from "./components/Header/Header";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import Account from "./pages/Account/Account";
+import Login from "./pages/Login/Login";
+import { authSlice } from "./store/Slices/AuthSlice";
 
-function App() {
+const App: React.FC = () => {
+  const { auth } = useAppSelector((state) => state.authReducer);
+  const dispatch = useAppDispatch();
+  const { rejectAuthorization } = authSlice.actions;
+
+  useEffect(() => console.log('render'))
+
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <MyHeader auth={auth} onClick={() => auth && dispatch(rejectAuthorization())}/>
+      <Content>
+        <Routes>
+          {auth ? (
+            <>
+              <Route path="/" element={<Login />} />
+              <Route path="/account" element={<Account />} />
+            </>
+          ) : (
+            <>
+              <Route path="*" element={<Login />} />
+            </>
+          )}
+        </Routes>
+      </Content>
+    </Layout>
   );
-}
+};
 
 export default App;
