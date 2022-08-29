c4094c2fe91426591d37b765a6de3292e982e925
pipirichi <mohiro@vk.com>
2022-08-29 20:17:52 +0300

    add test data

--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import ContactItem from '../ContactItem/ContactItem';
+import React from "react";
+import ContactItem from "../ContactItem/ContactItem";
 
 const ContactList = () => {
   return (
     <section>
-      <ContactItem id={0} name={'name'} surname={'surname'} telephone={'+7111222333'} email={'example@example.com'} description={'Good man'}/>
+      <ContactItem
+        id={0}
+        name={"name"}
+        surname={"surname"}
+        tel={"9999999999"}
+        email={"example@example.com"}
+        description={"Good man"}
+        deleteContact={() => {}}
+        patchData={() => {}}
+      />
+      <ContactItem
+        id={100}
+        name={"namenamenamenamenamenamename"}
+        surname={"surname"}
+        tel={"1111111111"}
+        email={"example@example.com"}
+        description={"Bad man"}
+        deleteContact={() => {}}
+        patchData={() => {}}
+      />
     </section>
   );
 };
