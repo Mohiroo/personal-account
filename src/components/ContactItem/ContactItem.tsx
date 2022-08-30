80dfddeec560005b5648ec9e146d5a8d37cfd96c
pipirichi <mohiro@vk.com>
2022-08-30 17:28:46 +0300

    renaming and response css

--- src/components/ContactItem/ContactItem.tsx
+++ src/components/ContactItem/ContactItem.tsx
@@ -13,10 +13,10 @@ interface ContactItemProps {
   email: string;
   description: string;
   deleteContact: () => void;
-  patchData: () => void;
+  sendChanges: () => void;
 }
 
-const ContactItem: React.FC<ContactItemProps> = ({ id, name, surname, tel, email, description, deleteContact, patchData }) => {
+const ContactItem: React.FC<ContactItemProps> = ({ id, name, surname, tel, email, description, deleteContact, sendChanges }) => {
   const [inputState, setInputState] = useState<IContacts>({
     id: id,
     name: name,
@@ -47,7 +47,7 @@ const ContactItem: React.FC<ContactItemProps> = ({ id, name, surname, tel, email
     if (!isValidTel() || !isValidEmail()) return;
 
     setInputState(editableData);
-    patchData();
+    sendChanges();
     setIsEditMode(false);
   };
 
@@ -55,65 +55,67 @@ const ContactItem: React.FC<ContactItemProps> = ({ id, name, surname, tel, email
 
   return (
     <article className="contact-item">
-      <form className="contact-item-info">
+      <form className="contact-item-card">
         <Button
           type="primary"
           shape="circle"
           size="small"
           icon={<RightOutlined />}
           onClick={() => setisDescription(!isDescription)}
-          className={"contact-item-button-description" + isDescription ? "-active" : ""}
+          className={"contact-item-button-description" + (isDescription ? "-active" : "")}
         />
-        <span className="contact-item-id">{inputState.id}</span>
-        {isEditMode ? (
-          <>
-            <Input
-              placeholder="Имя"
-              defaultValue={inputState.name}
-              onChange={(e) => setEditableData({ ...editableData, name: e.target.value })}
-              className="contact-item-unit"
-            />
-            <Input
-              placeholder="Фамилия"
-              defaultValue={inputState.surname}
-              onChange={(e) => setEditableData({ ...editableData, surname: e.target.value })}
-              className="contact-item-unit"
-            />
-            <Input
-              placeholder="Телефон"
-              defaultValue={inputState.tel}
-              onChange={(e) => setEditableData({ ...editableData, tel: e.target.value })}
-              maxLength={10}
-              className="contact-item-unit"
-              type={"tel"}
-              status={isValidTel() ? "" : "error"}
-            />
-            <Input
-              placeholder="Почта"
-              defaultValue={inputState.email}
-              onChange={(e) => setEditableData({ ...editableData, email: e.target.value })}
-              className="contact-item-unit"
-              status={isValidEmail() ? "" : "error"}
-            />
-            <div>
-              <Button type="link" onClick={(e) => confirmChanges()} icon={<CheckOutlined />} />
-              <Button type="text" onClick={undoChanges} icon={<CloseOutlined />} />
-            </div>
-          </>
-        ) : (
-          <>
-            <span className="contact-item-unit">{inputState.name}</span>
-            <span className="contact-item-unit">{inputState.surname}</span>
-            <span className="contact-item-unit">
-              +7 ({telPart(0, 3)}) {telPart(3, 6)}-{telPart(6, 8)}-{telPart(8, 11)}
-            </span>
-            <span className="contact-item-unit">{inputState.email}</span>
-            <div>
-              <Button type="link" onClick={() => setIsEditMode(true)} icon={<EditTwoTone />} />
-              <Button type="text" onClick={deleteContact} danger icon={<DeleteOutlined />} />
-            </div>
-          </>
-        )}
+        <div className="contact-item-info">
+          <span className="contact-item-id">{inputState.id}</span>
+          {isEditMode ? (
+            <>
+              <Input
+                placeholder="Имя"
+                defaultValue={inputState.name}
+                onChange={(e) => setEditableData({ ...editableData, name: e.target.value })}
+                className="contact-item-unit"
+              />
+              <Input
+                placeholder="Фамилия"
+                defaultValue={inputState.surname}
+                onChange={(e) => setEditableData({ ...editableData, surname: e.target.value })}
+                className="contact-item-unit"
+              />
+              <Input
+                placeholder="Телефон"
+                defaultValue={inputState.tel}
+                onChange={(e) => setEditableData({ ...editableData, tel: e.target.value })}
+                maxLength={10}
+                className="contact-item-unit"
+                type={"tel"}
+                status={isValidTel() ? "" : "error"}
+              />
+              <Input
+                placeholder="Почта"
+                defaultValue={inputState.email}
+                onChange={(e) => setEditableData({ ...editableData, email: e.target.value })}
+                className="contact-item-unit"
+                status={isValidEmail() ? "" : "error"}
+              />
+              <div>
+                <Button type="link" onClick={(e) => confirmChanges()} icon={<CheckOutlined />} />
+                <Button type="text" onClick={undoChanges} icon={<CloseOutlined />} />
+              </div>
+            </>
+          ) : (
+            <>
+              <span className="contact-item-unit">{inputState.name}</span>
+              <span className="contact-item-unit">{inputState.surname}</span>
+              <span className="contact-item-unit">
+                +7 ({telPart(0, 3)}) {telPart(3, 6)}-{telPart(6, 8)}-{telPart(8, 11)}
+              </span>
+              <span className="contact-item-unit">{inputState.email}</span>
+              <div>
+                <Button type="link" onClick={() => setIsEditMode(true)} icon={<EditTwoTone />} />
+                <Button type="text" onClick={deleteContact} danger icon={<DeleteOutlined />} />
+              </div>
+            </>
+          )}
+        </div>
       </form>
       {isDescription &&
         (isEditMode ? (
