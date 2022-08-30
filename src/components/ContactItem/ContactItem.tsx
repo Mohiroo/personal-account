1f82724cdb0683feb1591367fd94e0d68c7e7494
pipirichi <mohiro@vk.com>
2022-08-30 14:25:12 +0300

    end ContactItem (fix ckekings)

--- src/components/ContactItem/ContactItem.tsx
+++ src/components/ContactItem/ContactItem.tsx
@@ -1,13 +1,7 @@
-import {
-  CheckOutlined,
-  CloseOutlined,
-  DeleteOutlined,
-  EditTwoTone,
-  RightOutlined,
-} from "@ant-design/icons";
-import { Button, Input, Select } from "antd";
+import { CheckOutlined, CloseOutlined, DeleteOutlined, EditTwoTone, RightOutlined } from "@ant-design/icons";
+import { Button, Input } from "antd";
 import TextArea from "antd/lib/input/TextArea";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { IContacts } from "../../model/IAccount";
 import "./ContactItem.scss";
 
@@ -22,16 +16,7 @@ interface ContactItemProps {
   patchData: () => void;
 }
 
-const ContactItem: React.FC<ContactItemProps> = ({
-  id,
-  name,
-  surname,
-  tel,
-  email,
-  description,
-  deleteContact,
-  patchData,
-}) => {
+const ContactItem: React.FC<ContactItemProps> = ({ id, name, surname, tel, email, description, deleteContact, patchData }) => {
   const [inputState, setInputState] = useState<IContacts>({
     id: id,
     name: name,
@@ -44,24 +29,29 @@ const ContactItem: React.FC<ContactItemProps> = ({
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
   const [isDescription, setisDescription] = useState<boolean>(false);
 
-  const isValidTel = () => /^[0-9]/.test(editableData.tel);
-  // При вводе 1 цифры или букв тоже true
-  const isValidEmail = () =>
-    /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(editableData.email);
+  const isValidTel = () => /^[0-9]{10}$/.test(editableData.tel);
+  const isValidEmail = () => /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(editableData.email);
 
   const undoChanges = () => {
+    if (!isValidTel() || !isValidEmail()) {
+      setEditableData(inputState);
+      setIsEditMode(false);
+      return;
+    }
+
     setEditableData(inputState);
     setIsEditMode(false);
   };
 
   const confirmChanges = () => {
+    if (!isValidTel() || !isValidEmail()) return;
+
     setInputState(editableData);
     patchData();
     setIsEditMode(false);
   };
 
-  const telPart = (startNum: number, endNumber: number) =>
-    inputState.tel.substring(startNum, endNumber);
+  const telPart = (startNum: number, endNumber: number) => inputState.tel.substring(startNum, endNumber);
 
   return (
     <article className="contact-item">
@@ -72,56 +62,42 @@ const ContactItem: React.FC<ContactItemProps> = ({
           size="small"
           icon={<RightOutlined />}
           onClick={() => setisDescription(!isDescription)}
-          className={
-            "contact-item-button-description" + isDescription ? "-active" : ""
-          }
+          className={"contact-item-button-description" + isDescription ? "-active" : ""}
         />
         <span className="contact-item-id">{inputState.id}</span>
         {isEditMode ? (
           <>
             <Input
+              placeholder="Имя"
               defaultValue={inputState.name}
-              onChange={(e) =>
-                setEditableData({ ...editableData, name: e.target.value })
-              }
+              onChange={(e) => setEditableData({ ...editableData, name: e.target.value })}
               className="contact-item-unit"
             />
             <Input
+              placeholder="Фамилия"
               defaultValue={inputState.surname}
-              onChange={(e) =>
-                setEditableData({ ...editableData, surname: e.target.value })
-              }
+              onChange={(e) => setEditableData({ ...editableData, surname: e.target.value })}
               className="contact-item-unit"
             />
             <Input
+              placeholder="Телефон"
               defaultValue={inputState.tel}
-              onChange={(e) =>
-                setEditableData({ ...editableData, tel: e.target.value })
-              }
+              onChange={(e) => setEditableData({ ...editableData, tel: e.target.value })}
               maxLength={10}
               className="contact-item-unit"
               type={"tel"}
               status={isValidTel() ? "" : "error"}
             />
             <Input
+              placeholder="Почта"
               defaultValue={inputState.email}
-              onChange={(e) =>
-                setEditableData({ ...editableData, email: e.target.value })
-              }
+              onChange={(e) => setEditableData({ ...editableData, email: e.target.value })}
               className="contact-item-unit"
               status={isValidEmail() ? "" : "error"}
             />
             <div>
-              <Button
-                type="link"
-                onClick={(e) => confirmChanges()}
-                icon={<CheckOutlined />}
-              />
-              <Button
-                type="text"
-                onClick={undoChanges}
-                icon={<CloseOutlined />}
-              />
+              <Button type="link" onClick={(e) => confirmChanges()} icon={<CheckOutlined />} />
+              <Button type="text" onClick={undoChanges} icon={<CloseOutlined />} />
             </div>
           </>
         ) : (
@@ -129,22 +105,12 @@ const ContactItem: React.FC<ContactItemProps> = ({
             <span className="contact-item-unit">{inputState.name}</span>
             <span className="contact-item-unit">{inputState.surname}</span>
             <span className="contact-item-unit">
-              +7 ({telPart(0, 3)}) {telPart(3, 6)}-{telPart(6, 8)}-
-              {telPart(8, 11)}
+              +7 ({telPart(0, 3)}) {telPart(3, 6)}-{telPart(6, 8)}-{telPart(8, 11)}
             </span>
             <span className="contact-item-unit">{inputState.email}</span>
             <div>
-              <Button
-                type="link"
-                onClick={(e) => setIsEditMode(true)}
-                icon={<EditTwoTone />}
-              />
-              <Button
-                type="text"
-                onClick={deleteContact}
-                danger
-                icon={<DeleteOutlined />}
-              />
+              <Button type="link" onClick={() => setIsEditMode(true)} icon={<EditTwoTone />} />
+              <Button type="text" onClick={deleteContact} danger icon={<DeleteOutlined />} />
             </div>
           </>
         )}
@@ -154,9 +120,7 @@ const ContactItem: React.FC<ContactItemProps> = ({
           <TextArea
             rows={2}
             defaultValue={inputState.description}
-            onChange={(e) =>
-              setEditableData({ ...inputState, description: e.target.value })
-            }
+            onChange={(e) => setEditableData({ ...inputState, description: e.target.value })}
             className="contact-item-description"
           />
         ) : (
