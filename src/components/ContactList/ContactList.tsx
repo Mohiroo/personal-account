b5201fcb6a00b0b41939487e615225ed8f6b57ad
Mohiroo <mohiro@bk.ru>
2022-09-01 04:28:01 +0300

    ContactList refactoring + visual

--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -1,29 +1,31 @@
 import { Empty } from "antd";
 import React from "react";
-import { IContacts } from "../../model/IAccount";
+import { IContacts } from "../../model/IContacts";
 import ContactItem from "../ContactItem/ContactItem";
+import "./ContactList.scss";
 
 interface ContactListProps {
   contacts: IContacts[];
   deleteContact: (contact: IContacts) => void;
-  sendChanges: (contact: IContacts) => void;
+  unpdateContact: (contact: IContacts) => void;
 }
 
-const ContactList: React.FC<ContactListProps> = ({ contacts, deleteContact, sendChanges }) => {
+const ContactList: React.FC<ContactListProps> = ({ contacts, deleteContact, unpdateContact }) => {
   return (
-    <section>
+    <section className="contact-list">
       {contacts.length ? (
         contacts.map((contact) => (
           <ContactItem
             key={contact.id}
             id={contact.id}
+            userId={contact.userId}
             name={contact.name}
             surname={contact.surname}
             tel={contact.tel}
             email={contact.email}
             description={contact.description}
             deleteContact={deleteContact}
-            sendChanges={sendChanges}
+            unpdateContact={unpdateContact}
           />
         ))
       ) : (
